63d814294bc7076e23595f34364fae130a00c7b8
AARON HANNON - STUDENT <G00347352@gmit.ie>
2017-11-22 16:56:16 +0000

    Added placeholder buttons for the numbers, grid nearly complete

--- SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
+++ SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,46 @@ namespace SimpleCalculator
         public MainPage()
         {
             this.InitializeComponent();
+            createButtons();
+        }
+
+        private void createButtons()
+        {
+            StackPanel topStk = new StackPanel();
+            topStk.Background = new SolidColorBrush(Colors.SlateGray);
+            topStk.SetValue(Grid.RowProperty, 0);
+            MainHolder.Children.Add(topStk);
+
+            Grid btnGrid = new Grid();
+            btnGrid.Name = "btnGrid";
+
+            Button btn;
+
+            for (int i = 0; i < 3; i++)
+            {   
+                btnGrid.RowDefinitions.Add(new RowDefinition());
+                btnGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                for (int j = 0; j < 3; j++)
+                {
+                    btn = new Button();
+                    btn.Height = 25;
+                    btn.Width = 25;
+                    btn.VerticalAlignment = VerticalAlignment.Center;
+                    btn.HorizontalAlignment = HorizontalAlignment.Center;
+                    btn.Background = new SolidColorBrush(Colors.White);
+                    btn.SetValue(Grid.RowProperty, i);
+                    btn.SetValue(Grid.ColumnProperty, j);
+
+                    btnGrid.Children.Add(btn);
+                }
+
+
+            }
+
+            btnGrid.Background = new SolidColorBrush(Colors.Black);
+            btnGrid.SetValue(Grid.RowProperty, 1);
+            MainHolder.Children.Add(btnGrid); 
         }
     }
 }
