a0b0d61452e0659673066002dbef4d0eb65787b5
AARON HANNON - STUDENT <G00347352@gmit.ie>
2017-12-16 15:39:43 +0000

    Display Working

--- SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
+++ SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,25 +24,43 @@ namespace SimpleCalculator
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int numberCounter = 1;
+        string numString;
+        Button btn;
+        bool started = false;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            //ApplicationView.PreferredLaunchViewSize = new Size(720, 1280);
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(720, 1280));
             createButtons();
         }
 
         private void createButtons()
         {
             StackPanel topStk = new StackPanel();
-            topStk.Background = new SolidColorBrush(Colors.SlateGray);
+            topStk.Background = new SolidColorBrush(Colors.DarkSlateGray);
             topStk.SetValue(Grid.RowProperty, 0);
             MainHolder.Children.Add(topStk);
+
+            Grid displayGrid = new Grid();
+            displayGrid.Name = "DisplayGrid";
+            displayGrid.Height = 300;
+            displayGrid.Width = 500;
+
+            for (int k = 0; k < 3; k++)
+            {
+                displayGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            topStk.Children.Add(displayGrid);
 
             Grid btnGrid = new Grid();
             btnGrid.Name = "btnGrid";
 
-            Button btn;
-
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 4; i++)
             {   
                 btnGrid.RowDefinitions.Add(new RowDefinition());
                 btnGrid.ColumnDefinitions.Add(new ColumnDefinition());
@@ -49,23 +68,96 @@ namespace SimpleCalculator
                 for (int j = 0; j < 3; j++)
                 {
                     btn = new Button();
-                    btn.Height = 25;
-                    btn.Width = 25;
+                    btn.Height = 150;
+                    btn.Width = 150;
                     btn.VerticalAlignment = VerticalAlignment.Center;
                     btn.HorizontalAlignment = HorizontalAlignment.Center;
-                    btn.Background = new SolidColorBrush(Colors.White);
+                    //btn.Background = new SolidColorBrush(Colors.White);
                     btn.SetValue(Grid.RowProperty, i);
                     btn.SetValue(Grid.ColumnProperty, j);
 
-                    btnGrid.Children.Add(btn);
-                }
+                    string value ="";
 
+                    if (numberCounter < 10)
+                    {
+                        btn.Content = numberCounter;
+                        value += numberCounter;
+                        btn.Tag = value;
+                    }
+                    else
+                    {
+                        switch (numberCounter)
+                        {
+                            case 10:
+                                btn.Content = ".";
+                                value += numberCounter;
+                                btn.Tag = value;
+                                break;
+
+                            case 11:
+                                btn.Content = 0;
+                                break;
+
+                            case 12:
+                                btn.Content = "=";
+                                break;
+                        }
+                    }
+
+                    btn.FontFamily = new FontFamily("Sans-Serif");
+                    btn.FontSize = 40;
+                    btn.Foreground = new SolidColorBrush(Colors.White);
+                    btn.BorderThickness = new Thickness(0);
+
+                    btn.Tapped += Btn_Tapped;
+                    btnGrid.Children.Add(btn);
+
+                    numberCounter++;
+                }
 
             }
 
-            btnGrid.Background = new SolidColorBrush(Colors.Black);
+            
+            System.Diagnostics.Debug.WriteLine("Tapped: ");
+
+            btnGrid.Background = new SolidColorBrush(Color.FromArgb(255, (byte)0, (byte)38, (byte)45));
             btnGrid.SetValue(Grid.RowProperty, 1);
-            MainHolder.Children.Add(btnGrid); 
+            MainHolder.Children.Add(btnGrid);
+            
         }
+
+        TextBlock txBl = new TextBlock();
+
+        private void Btn_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+     
+            string value = " ";    
+            Button btn = (Button)sender;
+            Grid outputGrid = FindName("DisplayGrid") as Grid;
+
+            if (started)
+            {
+                outputGrid.Children.Remove(txBl);
+            }
+
+            value = (string)btn.Tag;
+
+            numString += value;
+            //txBl = null;
+
+            txBl.Text = numString;
+            txBl.SetValue(Grid.RowProperty, 1);
+            txBl.HorizontalAlignment = HorizontalAlignment.Right;
+            txBl.Foreground = new SolidColorBrush(Colors.White);
+            txBl.FontSize = 45;
+
+            System.Diagnostics.Debug.WriteLine("Tapped: " + value);
+            outputGrid.Children.Add(txBl);
+
+            started = true;
+
+            //txBl.Text = value;
+        }
+
     }
 }
